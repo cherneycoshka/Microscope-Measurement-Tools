69a58cc4886726f21ad0b3b05844944806107c16
JP Hadden <jpe.hadden@gmail.com>
2020-03-31 01:40:12 +0100

    Trimmed repeated files.

--- Analyze/Microscope Measurement Tools-Edited/Microscope_Calibrations_user_settings.py
+++ Analyze/Microscope Measurement Tools-Edited/Microscope_Calibrations_user_settings.py
@@ -1,23 +1,14 @@
 '''
-	FIJI Plugin - Microscope Measurement Tools
-        Configuration for Choose_Microscope_Calibration.py & Draw_Measurement_-_Line.py
-        Demis D. John, University of California Santa Barbara, Nanofabrication Facility, 2019
-        https://www.nanotech.ucsb.edu
+	FIJI Plugin
+Configuration for Microscope Calibrations
+Demis D. John, Univ. of California Santa Barbara, 2019
 
-Make sure the lists `names`, `cals`, `units` and `aspect_ratio` all have the same number of items!
+This version of the settings file contains a pointer to a custom function, which automatically sets the scale by loading a text file that corresponds to the loaded image file.  The custom function is found in a separate file "JEOL_SEM_AutoCal.py", and you can see the class defined in this file being loaded below.
+In addition, an instance of the class is inserted into the lists of names/calibrations, and all information will be obtained from the Class/external file.
+
+Please make sure the lists `names`, `cals` and `units` all have the same number of items!
 
 After you edit this file, be sure to delete the corresponding *.pyclass file and restart Fiji for the settings to take effect.
-
-
-All of these lists are regular Python Lists, so define them however you prefer to define pythonic lists.  You can define them in-line like normal:
-    x = [ 'one', 'two',  'three' ]
-or in a vertical fashion:
-    x = [
-            'one',      # you can even comment on each line of the list
-            'two',
-            'three'
-        ]
-or even do python math on the lists, such as repeating an item with `*`, or using "list comprehensions".
 '''
 
 
@@ -29,7 +20,16 @@ or even do python math on the lists, such as repeating an item with `*`, or usin
 Microscope scaling/pixel-size calibration settings.
 """
 
-# The names of the microscope calibrations (shows up as the radio button/drop-down names):
+# The names of the microscope calibrations (shows up as the radio button names):
+#   For custom functions, pass the handle to an instantiated class instead of a string.
+
+# Custom Function: Load JEOL SEM autocal class from a file:
+from JEOL_SEM_AutoCal   import JEOL_SEM_CalFromTxt
+from JPH_SEM_AutoCal   import JPH_SEM_CalFromTxt
+
+jeol_sem_cal_from_txt = JEOL_SEM_CalFromTxt()    # instantiate the class
+jph_sem_cal_from_txt = JPH_SEM_CalFromTxt()    # instantiate the class
+
 names = [
         'FluoroScope 5x', 
         'FluoroScope 20x', 
@@ -37,37 +37,55 @@ names = [
         'FluoroScope 100x', 
         'FluoroScope 150x',
         'Olympus DUV 100x',
+        jeol_sem_cal_from_txt, # instance of class with custom function `MyClass.cal()` for setting image scale.
+		jph_sem_cal_from_txt, # placeholder for custom function
         ]
 
 
-# The 'pixel-per-unit' obtained from the "Set Scale..." dialog, for each named calibration above:
+# The 'pixel-per-unit' obtained from the "Set Scale..." Dialog, for each named calibration above:
 cals = [
-        0.9058,     # FluoroScope 5x to 150x
+        0.9058,
         1.81,  
         4.525,  
         9.0667,  
         13.5333,
-        54.6875,    # Olympus DUV
+        54.6875,
+        jeol_sem_cal_from_txt,  # placeholder for custom function
+		jph_sem_cal_from_txt, # placeholder for custom function
         ]
 #   This is just 1/pixel_width, in case you were wondering.
 
 
 # length-units for each calibration:
-#units = ['um','um','um','um','um']
-# for identical units for all cals, can use the following line instead (uncomment):
-units = ['um'    for x in cals]    # list-comprehension with constant `um`
+units = [
+        'um',
+        'um',
+        'um',
+        'um',
+        'um',
+        'um',
+        jeol_sem_cal_from_txt,
+		jph_sem_cal_from_txt,
+        ]
+# for identical units for all cals, use the following line instead (uncomment):
+#units = ['um'    for x in cals]    # list-comprehension with constant `um`
 
 
 # Aspect Ratio for each calibration (default should be 1.0 )
 #   Ratio is defined as so: pixelHeight = pixelWidth * AspectRatio
-# The following sets the aspect ratio to 1 for all calibrations:
-aspect_ratio =  [ 1.0    for x in cals ]   # list-comprehension with constant `1`
 # Uncomment the following line to use custom aspect ratios:
-#aspect_ratio = [1, 1, 1, 1, 1]
-
-
-
-
+aspect_ratio = [
+        1, 
+        1, 
+        1, 
+        1, 
+        1,
+        1,
+        jeol_sem_cal_from_txt,
+		jph_sem_cal_from_txt,
+                ]
+# The following sets the aspect ratio to 1 for all calibrations (uncomment to use):
+#aspect_ratio =  [ 1.0    for x in cals ]   # list-comprehension with constant `1`
 
 """
 ################################
@@ -104,7 +122,7 @@ texttoleft = True      # put text on left or right side of last point?
 
 '''
 ---------------------------------------------------------
-Warn user if they erroneously run this file as a stand-alone plugin.
+Warn user if they run this file as a stand-alone plugin.
 '''
 
 def run():
